adf7f35e40f5938d1f3a3b9ac26c46623dfd1ba9
skylide <dupont.arnaud.58@gmail.com>
2019-04-12 14:34:23 +0200

    Add files via upload

--- skydream/footer.php
+++ skydream/footer.php
@@ -18,6 +18,8 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
 	integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
 </script>
+<script src="theme/skydream/scripts.js">
+</script>
 </body>
 
 </html>
--- skydream/header.php
+++ skydream/header.php
@@ -18,6 +18,7 @@ include_once(THEMES.$core->getConfigVal('theme').'/functions.php');
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
 		integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 	<link rel="stylesheet" href="theme/skydream/styles.css">
+	<?php show::scriptTags(); ?>
 	<link href="https://fonts.googleapis.com/css?family=PT+Sans+Caption|Permanent+Marker" rel="stylesheet">
 	<?php eval($core->callHook('endFrontHead')); ?>
 </head>
