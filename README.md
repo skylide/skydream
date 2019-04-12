91524c4059cd230ab8efbf9649d14516a4629d3c
skylide <dupont.arnaud.58@gmail.com>
2019-04-12 14:57:11 +0200

    modification

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # skydream
 skydream est un theme pour le cms 99ko cms.
+Guide d'installation :
+Glisser & déposer les fichiers a la racine de 99ko cms.
--- theme/skydream/header.php
+++ theme/skydream/header.php
@@ -17,7 +17,7 @@ include_once(THEMES.$core->getConfigVal('theme').'/functions.php');
 		integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
 		integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
-	<link rel="stylesheet" href="theme/skydream/styles.css">
+	<?php show::linkTags(); ?>
 	<?php show::scriptTags(); ?>
 	<link href="https://fonts.googleapis.com/css?family=PT+Sans+Caption|Permanent+Marker" rel="stylesheet">
 	<?php eval($core->callHook('endFrontHead')); ?>
